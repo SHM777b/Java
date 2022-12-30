a94e201fadcec16290e0b6d8af8e7b9b7c14540b
Murad <37251399+SHM777b@users.noreply.github.com>
2022-12-30 16:40:21 +0100

    Update TeenNumberChecker.java

--- Expression and Statements/TeenNumberChecker.java
+++ Expression and Statements/TeenNumberChecker.java
@@ -1,3 +1,9 @@
+/* 
+We'll say that a number is "teen" if it is in the range 13 -19 (inclusive).
+Write a method named hasTeen with 3 parameters of type int.
+The method should return boolean and it needs to return true if one of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+*/
+
 public class TeenNumberChecker {
 
     public static boolean hasTeen(int firstInteger, int secondInteger, int thirdInteger) {
