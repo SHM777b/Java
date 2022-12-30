5a353468908f809aa86dd2e41bce5f7ee4fd81da
Murad <37251399+SHM777b@users.noreply.github.com>
2022-12-30 16:39:59 +0100

    Update EqualSumChecker.java

--- Expression and Statements/EqualSumChecker.java
+++ Expression and Statements/EqualSumChecker.java
@@ -1,3 +1,8 @@
+/* 
+Write a method hasEqualSum with 3 parameters of type int.
+The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+*/
+
 public class EqualSumChecker {
 
     public static boolean hasEqualSum(int first, int second, int third) {
