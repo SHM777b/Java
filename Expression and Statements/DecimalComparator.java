0c5029f8ee7051ff2d29225a8c15e340009fcc24
Murad <37251399+SHM777b@users.noreply.github.com>
2022-12-30 16:39:35 +0100

    Update DecimalComparator.java

--- Expression and Statements/DecimalComparator.java
+++ Expression and Statements/DecimalComparator.java
@@ -1,3 +1,8 @@
+/* 
+Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
+*/
+
 public class DecimalComparator {
 
     public static boolean areEqualByThreeDecimalPlaces(double firstVariable, double secondVariable) {
