c259b71fca9f4d33345c86d98f726b9b5c63db3c
Murad <37251399+SHM777b@users.noreply.github.com>
2023-01-27 18:22:04 +0100

    Create Monster.java

---
+++ Java Programming Masterclass/Interfaces/Interface Challenge/Monster.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISaveable{
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster (String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> listOfFields = new ArrayList<String>();
+        listOfFields.addAll(List.of(name, "" + hitPoints, "" + strength));
+        return listOfFields;
+    }
+
+    @Override
+    public void read(List<String> list) {
+        if (list != null && list.size() > 0) {
+            name = list.get(0);
+            hitPoints = Integer.parseInt(list.get(1));
+            strength = Integer.parseInt(list.get(2));
+        }
+    }
+}
